9b50cd20be5fa8a24d8e12a0e0563246fe6941c5
tdnogueira <nogueira.tcs@gmail.com>
2020-06-27 03:11:18 -0300

    desafio concluido

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,80 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const findRepositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (findRepositoryId === -1) {
+    return response.status(400).json({ error: 'Repositorio não existe.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepositoryId].likes,
+  };
+
+  repositories[findRepositoryId] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const findRepositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (findRepositoryId >= 0) {
+    repositories.splice(findRepositoryId, 1);
+  } else {
+    return response.status(400).json({ error: 'Repositorio não existe.' });
+  }
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const findRepositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (findRepositoryId === -1) {
+    return response.status(400).json({ error: 'Repositorio não existe.' });
+  }
+
+  repositories[findRepositoryId].likes++;
+
+  return response.json(repositories[findRepositoryId]);
 });
 
 module.exports = app;
